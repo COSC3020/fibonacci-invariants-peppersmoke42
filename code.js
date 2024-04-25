498cf9604d601f95ab8ede268c42d37983938a0e
peppersmoke42 <peterchr102@gmail.com>
2024-04-24 22:59:24 -0600

    Submission

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function fib(n) {
-    return n;
+    let fibAns = new Array(n + 1);
+    if (n > 1)
+    {
+        let temp = fib(n - 1)
+        for (let i = 0; i < temp.length; i++)
+            fibAns[i] = temp[i]
+        fibAns[fibAns.length - 1] = fibAns[fibAns.length - 2] + fibAns[fibAns.length - 3]
+    }
+    else if (n <= 1)
+        for (let i = 0; i <= n; i++)
+            fibAns[i] = i;
+
+    return fibAns;
 }
