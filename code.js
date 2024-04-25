fd9ad50ebfa68d4230c619df864d7ac497bc420d
peppersmoke42 <peterchr102@gmail.com>
2024-04-24 23:11:44 -0600

    Only needs 1 comparison

--- code.js
+++ code.js
@@ -1,15 +1,12 @@
 function fib(n) {
     let fibAns = new Array(n + 1);
-    if (n > 1)
-    {
+    if (n > 1) {
         let temp = fib(n - 1)
         for (let i = 0; i < temp.length; i++)
             fibAns[i] = temp[i]
         fibAns[fibAns.length - 1] = fibAns[fibAns.length - 2] + fibAns[fibAns.length - 3]
-    }
-    else if (n <= 1)
+    } else
         for (let i = 0; i <= n; i++)
             fibAns[i] = i;
-
     return fibAns;
 }
